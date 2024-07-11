8a9bb001f53d88dc806fa65e15cbdaf293fbc8c3
Berlin <evan1234.ek@gmail.com>
2024-07-11 23:48:35 +0600

    chore: Update database and create issue api

--- prisma/migrations/20240711174702_create_issue/migration.sql
+++ prisma/migrations/20240711174702_create_issue/migration.sql
@@ -5,7 +5,7 @@ CREATE TYPE "Status" AS ENUM ('OPEN', 'IN_PROGRESS', 'CLOSED');
 CREATE TABLE "Issue" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(255) NOT NULL,
-    "description" TEXT,
+    "description" TEXT NOT NULL,
     "status" "Status" NOT NULL DEFAULT 'OPEN',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
