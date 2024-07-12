e523321b57fbfd7aded08a698f1ef47763b6b943
Berlin <evan1234.ek@gmail.com>
2024-07-12 14:04:28 +0600

    build new issue page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const Home = () => {
   return (
     <div>
-      <Button>Click me</Button>
+      <Button>
+        <Link href="/issues/new">Create Issue</Link>
+      </Button>
       <h1>Hello World</h1>
     </div>
   );
