a6a128ac624d1fef8f2dbd58cd27cf5fea30d9c8
Berlin <evan1234.ek@gmail.com>
2024-07-12 23:28:34 +0600

    show the issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import { Button } from '@radix-ui/themes';
-import Link from 'next/link';
-
 const Home = () => {
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">Create Issue</Link>
-      </Button>
       <h1>Hello World</h1>
     </div>
   );
