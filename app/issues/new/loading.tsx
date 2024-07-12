4c811eb2602299fbe6c2bb584123efb3d5f02999
Berlin <evan1234.ek@gmail.com>
2024-07-13 00:17:25 +0600

    refactor: Remove unused imports and optimize code in issue pages

---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingNewIssuePage = () => {
+  return (
+    <div>
+      <p className="text-2xl font-semibold text-gray-800">Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingNewIssuePage;
