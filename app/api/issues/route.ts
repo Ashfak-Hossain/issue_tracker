726fcc855d6e6148ffab90d4ff4fc9a9250e04ac
Berlin <evan1234.ek@gmail.com>
2024-07-12 22:57:43 +0600

    implement client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,14 +1,9 @@
+import { createIssueSchema } from '@/app/validationSchema';
 import prisma from '@/lib/client';
 import { NextRequest, NextResponse } from 'next/server';
-import { z } from 'zod';
 
-const createIssueSchema = z.object({
-  title: z.string().min(1, 'Title is required').max(255),
-  description: z.string().min(1, "Description can't be empty"),
-});
-
-export async function POST(req: NextRequest) {
-  const body = await req.json();
+export async function POST(request: NextRequest) {
+  const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,24 +1,30 @@
 'use client';
 
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { Controller, useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { error } from 'console';
 import { BiErrorCircle } from 'react-icons/bi';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
   const [error, setError] = useState('');
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
   return (
     <div className="max-w-xl">
@@ -43,6 +49,11 @@ const NewIssuePage = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register('title')} />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -50,6 +61,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text as="p" color="red">
+            {errors.description.message}
+          </Text>
+        )}
 
         <Button>Submit New Issue</Button>
       </form>
