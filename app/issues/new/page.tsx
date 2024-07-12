4b1ccb446de033aa3ab38dc3deb371fb1bf9ced9
Berlin <evan1234.ek@gmail.com>
2024-07-12 23:19:05 +0600

    reafactor: move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -29,6 +29,17 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setIsSubmitting(false);
+      setError('An error occurred.');
+    }
+  });
+
   return (
     <div className="max-w-xl">
       {error && (
@@ -40,19 +51,7 @@ const NewIssuePage = () => {
         </Callout.Root>
       )}
 
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setIsSubmitting(false);
-            setError('An error occurred.');
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root placeholder="Title" {...register('title')} />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
 
